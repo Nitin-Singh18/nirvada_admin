8398285d95fd543e728a5726add308066e422340
Nitin Singh <i.nitinsingh18@gmail.com>
2023-03-15 16:54:41 +0530

    Set Up

---
+++ android/app/src/main/java/com/nirvada_admin/nirvada_admin/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nirvada_admin.nirvada_admin;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Nirvāda",
+          theme: ThemeData(fontFamily: "Poppins"),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      },
+    ),
+  );
+}
